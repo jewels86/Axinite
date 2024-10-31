cc176e918a626f45a19543879bca729349fd20f5
jewels86 <jewels286@proton.me>
2024-10-31 09:00:12 -0400

    going to try this again

--- test.py
+++ test.py
@@ -2,8 +2,10 @@ import axinite
 
 cmpdA1 = axinite.CompoundA(axinite.m(1), axinite.m(2), axinite.m(3))
 cmpdA2 = axinite.CompoundA(axinite.km(1))
+cmpdS1 = axinite.CompoundS(axinite.m(1), axinite.m(2), axinite.m(3))
+cmpdS2 = axinite.CompoundS(axinite.km(1))
 
-print(cmpdA1)
-print(cmpdA2)
-print(cmpdA1 + cmpdA2)
-print(axinite.m(1) + axinite.km(1))
+print(cmpdS1)
+print(cmpdS2)
+print(cmpdS1 + cmpdS2)
+print(cmpdS1 + cmpdA1)
