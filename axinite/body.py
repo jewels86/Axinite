e94fb1f3e63d99b780ad9ddaafe6569385821bc5
jewels86 <jewels286@proton.me>
2024-11-01 19:40:12 -0400

    started bodies

--- axinite/body.py
+++ axinite/body.py
@@ -1,6 +1,13 @@
 from misura.quantities import quantity
+from trimesh import Trimesh
 
 class Body:
-    def __init__(self, mass: quantity, volume: quantity, label: str, ):
+    def __init__(self, mesh: Trimesh, mass: quantity, volume: quantity, label: str, ):
+        self.mesh = mesh.apply_scale((volume / mesh.volume) ** (1/3))
+        
         self.mass = mass
         self.volume = volume
+        self.density = mass / volume
+        
+        self.average_radius = (mesh.bounding_sphere.primitive.radius + mesh.bounding_box_oriented.primitive.extents.max() / 2) / 2
+        self.label = label
