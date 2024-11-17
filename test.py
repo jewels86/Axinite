2ea2c9597e767b9fb910e696176f86597e21b284
Jewels <jewels286@proton.me>
2024-11-16 22:15:59 -0500

    2 body works

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import astropy.units as u
 from astropy.coordinates import CartesianRepresentation
 from astropy.constants import G
+import json
 
 m_earth = 5.972e24 * u.kg
 r0_earth = CartesianRepresentation([0, 0, 0] * u.meter)
@@ -11,15 +12,15 @@ v0_earth = CartesianRepresentation([0, 0, 0] * u.meter / u.s)
 
 m_moon = 7.342e22 * u.kg
 r0_moon = CartesianRepresentation([3.844e8, 0, 0] * u.meter)
-v0_moon = CartesianRepresentation([0, 1022, 0] * u.meter / u.s)
+v0_moon = CartesianRepresentation([-1022, -1022, 1022] * u.meter / u.s)
 
 def g(r: CartesianRepresentation): 
-    return -((G * m_earth) / r.magnitude**3) * r
+    return -((G * m_earth) / r.norm()**3) * r
 
-r_moon = {
+r = {
     0 * u.s: r0_moon,
 }
-v_moon = {
+v = {
     0 * u.s: v0_moon,
 }
 a = {
@@ -30,8 +31,26 @@ f = {
 }
 
 t = 0 * u.second
-delta = (1 * u.hour).to(u.second)
+delta = 10 * u.second
 limit = (30 * u.day).to(u.second)
-
+i = 0
+print("Starting...")
 while t < limit:
+    f[t + delta] = g(r[t]) * m_moon
+    a[t + delta] = f[t + delta] / m_moon
+    v[t + delta] = v[t] + (a[t + delta] * delta)
+    r[t + delta] = r[t] + (v[t + delta] * delta)
+    print(f"Timestep: {t} - {t.to(u.day).value:.2f} days ({r[t + delta].x.value:.2f}, {r[t + delta].y.value:.2f}, {r[t + delta].z.value:.2f})", end="\r")
     
+    i += 1
+    t += delta
+print(f"Computed {i} timesteps ({t.to(u.day).value} days).")
+data = {
+    'f': {str(k): v.xyz.value.tolist() for k, v in f.items()},
+    'a': {str(k): v.xyz.value.tolist() for k, v in a.items()},
+    'v': {str(k): v.xyz.value.tolist() for k, v in v.items()},
+    'r': {str(k): v.xyz.value.tolist() for k, v in r.items()},
+}
+
+with open('text.ax', 'w') as file:
+    json.dump(data, file)
