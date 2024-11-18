Fixed the gravitational acceleration formula?

test.py
@@ -18,9 +18,6 @@ m_moon = 7.342e22 * u.kg
 r0_moon = CartesianRepresentation([3.844e8, 0, 0] * u.meter)
 v0_moon = CartesianRepresentation([-1022, -1022, 1022] * u.meter / u.s)
 
-def g(r: CartesianRepresentation): 
-    return -((G * m_earth) / r.norm()**3) * r
-
 r = {
     0 * u.s: r0_moon,
 }
@@ -29,9 +26,6 @@ v = {
 }
 a = {
     0 * u.s: CartesianRepresentation([0, 0, 0] * u.meter / u.s**2),
-}
-f = {
-    0 * u.s: g(r0_moon) * m_moon,
 }
 
 t = 0 * u.second
@@ -47,17 +41,17 @@ if os.path.exists('./test.ax'):
         r = {u.Quantity(float(k), u.s): CartesianRepresentation(np.array(v) * u.meter) for k, v in data['r'].items()}
         v = {u.Quantity(float(k), u.s): CartesianRepresentation(np.array(v) * u.meter / u.s) for k, v in data['v'].items()}
         a = {u.Quantity(float(k), u.s): CartesianRepresentation(np.array(v) * u.meter / u.s**2) for k, v in data['a'].items()}
-        f = {u.Quantity(float(k), u.s): CartesianRepresentation(np.array(v) * u.meter / u.s**2) for k, v in data['f'].items()}
 else:
     while t < limit:
-        f[t + delta] = g(r[t]) * m_moon
-        a[t + delta] = f[t + delta] / m_moon
+        a[t + delta] = ((-G * m_earth) / ax.vector_magnitude(r[t])**3) * r[t]
         v[t + delta] = v[t] + (a[t + delta] * delta)
         r[t + delta] = r[t] + (v[t + delta] * delta)
         print(f"Timestep: {t} - {t.to(u.day).value:.2f} days ({r[t + delta].x.value:.2f}, {r[t + delta].y.value:.2f}, {r[t + delta].z.value:.2f})", end="\r")
         
         i += 1
         t = t + delta
+        
+    print(r[t])
 
     with open('test.ax', 'w') as file:
         data = {
@@ -87,4 +81,4 @@ def update(frame):
     return line
 
 ani = animation.FuncAnimation(fig, update, frames=len(x_positions), repeat=False, interval=1)
-plt.show()
+plt.show