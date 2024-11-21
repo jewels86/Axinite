78ffe5c8017bfd1470d52bacc65eb603ed5e57a5
Jewels <jewels286@proton.me>
2024-11-21 16:29:30 -0500

    clean slate

--- axinite/__init__.py
+++
@@ -1,5 +0,0 @@
-from axinite.body import Body
-from axinite.functions import apply_to_vector, vector_to, vector_magnitude
-from axinite.commands import load, show
-import axinite.shapes as shapes
-import axinite.functions as functions
