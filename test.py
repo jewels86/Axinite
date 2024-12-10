3d1bb2cd3421abc52fc16f20d2e08bec7351890e
jewels86 <jewels286@proton.me>
2024-12-10 08:09:06 -0500

    updated gitignore

--- test.py
+++
@@ -1,8 +0,0 @@
-import axinite as ax
-import axtools as axt
-import astropy.units as u
-
-args = axt.read("templates/solar-system.tmpl.ax")
-bodies = axt.load(args, "solar-system.ax")
-args = axt.read("solar-system.ax")
-axt.show(args.t.value, args.delta.value, *args.bodies, radius_multiplier=args.radius_multiplier, retain=args.retain)
