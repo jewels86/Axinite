30571883b0a2c1609615849edadb632afadbdd53
Jewels <jewels286@proton.me>
2024-12-22 14:17:15 -0500

    small changes - also, dont use jit

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ import sys
 
 if sys.argv[1] == "load":
     args = axtools.read(sys.argv[2])
-    axtools.load(args, sys.argv[3], jit=True)
+    axtools.load(args, sys.argv[3], jit=False)
 elif sys.argv[1] == "show":
     args = axtools.read(sys.argv[2])
-    axtools.show(args, axtools.plotly_frontend(args, "show"))
+    axtools.show(args, axtools.plotly_frontend(
+        args, "show", 
+        use_min=args.frontend_args["plotly"]["use_min"] if "plotly" in args.frontend_args and "use_min" in args.frontend_args["plotly"] else False))
 elif sys.argv[1] == "live":
     args = axtools.read(sys.argv[2])
     axtools.live(args, axtools.vpython_frontend(args, "live"))
