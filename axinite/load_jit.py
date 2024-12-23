50ad7a3605981a18c38e710bcbccbde8adc42766
Jewels <jewels286@proton.me>
2024-12-23 03:04:37 +0000

    attempting some more jit stuff

--- axinite/load_jit.py
+++ axinite/load_jit.py
@@ -2,7 +2,8 @@ import numpy as np
 from numba import njit, typed, types
 
 G = 6.67430e-11
-@njit
+
+@njit 
 def vector_magnitude(vec):
     return np.sqrt(np.sum(vec**2))
 
@@ -12,15 +13,12 @@ def unit_vector(vec):
     return vec / mag if mag > 0 else vec
 
 @njit
-def gravitational_force(mass1, mass2, r):
-    return -G * ((mass1 * mass2) / vector_magnitude(r)**2) * unit_vector(r)
+def gravitational_force(m1, m2, r):
+    mag = vector_magnitude(r)
+    if mag == 0:
+        return np.zeros(3)
+    return -G *((m1 * m2) / mag**2) * unit_vector(r)
 
 @njit
-def load(delta, limit, action, bodies, t=0.0, modifiers=None):
-    if modifiers is None: modifiers = typed.List.empty_list(types.ListType(types.float64[:]))
-    while t < limit:
-        for i, body in enumerate(bodies):
-            f = np.zeros(3)
-            for j, other in enumerate(bodies):
-                if i != j:
-                    r_diff = body[""]
+def _load_jit(delta, limit, bodies):
+    
