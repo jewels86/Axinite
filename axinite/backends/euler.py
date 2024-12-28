42ef0b34449ca488bd0f005fe6b9b1f35bdfedb5
Jewels <jewels286@proton.me>
2024-12-27 21:19:51 -0500

    refactored the backends

--- axinite/backends/euler.py
+++ axinite/backends/euler.py
@@ -2,8 +2,7 @@ import numpy as np
 from numba import njit, typed, types, jit
 import axinite as ax
 
-@jit(nopython=False)
-def euler_backend(delta, limit, bodies, action=None, modifier=None, t=-1.0, action_frequency=200):
+def euler_nojit_backend(delta, limit, bodies, action=None, modifier=None, t=-1.0, action_frequency=200):
     if t == -1.0: t = 0.0 + delta
     n = 1
     while t < limit:
@@ -24,23 +23,6 @@ def euler_backend(delta, limit, bodies, action=None, modifier=None, t=-1.0, acti
         if action is not None and n % action_frequency == 0: action(bodies, t)
     return bodies
 
+@jit(nopython=False)
 def euler_backend(delta, limit, bodies, action=None, modifier=None, t=-1.0, action_frequency=200):
-    if t == -1.0: t = 0.0 + delta
-    n = 1
-    while t < limit:
-        for i, body in enumerate(bodies):
-            f = np.zeros(3)
-            for j, other in enumerate(bodies):
-                if i != j:
-                    r = body["r"][n - 1] - other["r"][n - 1]
-                    f += ax.gravitational_force_jit(body["m"], other["m"], r)
-            if modifier is not None: f = modifier(body, f)
-            a = f / body["m"]
-            v = body["v"][n - 1] + a * delta
-            r = body["r"][n - 1] + v * delta
-            body["v"][n] = v
-            body["r"][n] = r
-        t += delta
-        n += 1
-        if action is not None: action(t, limit=limit, bodies=bodies, delta=delta)
-    return bodies
+    return jit(euler_nojit_backend, nopython=True)(delta, limit, bodies, action=action, modifier=modifier, t=t, action_frequency=action_frequency)
--- axinite/backends/verlet.py
+++ axinite/backends/verlet.py
@@ -2,8 +2,7 @@ import numpy as np
 from numba import njit, typed, types, jit
 import axinite as ax
 
-@jit(nopython=False)
-def verlet_backend(delta, limit, bodies, action=None, modifier=None, t=0.0, action_frequency=200):
+def verlet_nojit_backend(delta, limit, bodies, action=None, modifier=None, t=0.0, action_frequency=200):
     if t != 0.0: raise Exception("Verlet method does not support non-zero initial time.")
     t = 0.0 + delta 
     n = 1
@@ -33,31 +32,6 @@ def verlet_backend(delta, limit, bodies, action=None, modifier=None, t=0.0, acti
     
     return bodies
 
-def verlet_nojit_backend(delta, limit, bodies, action=None, modifier=None, t=0.0):
-    if t != 0.0: raise Exception("Verlet method does not support non-zero initial time.")
-    t = 0.0 + delta 
-    n = 1
-
-    for i, body in enumerate(bodies):
-        f = np.zeros(3)
-        for j, other in enumerate(bodies):
-            if i != j: f += ax.gravitational_force_jit(body["m"], other["m"], body["r"][0] - other["r"][0])
-        body["r"][1] = body["r"][0] + body["v"][0] * delta + 0.5 * (f / body["m"]) * delta**2
-        body["v"][1] = (body["r"][1] - body["r"][0]) / (2 * delta)
-    
-    n += 1
-    t += delta
-
-    while t < limit:
-        for i, body in enumerate(bodies):
-            f = np.zeros(3)
-            for j, other in enumerate(bodies):
-                if i != j: f += ax.gravitational_force_jit(body["m"], other["m"], body["r"][n-1] - other["r"][n-1])
-
-            body["r"][n] = body["r"][n-1] * 2 - body["r"][n-2] + (f / body["m"]) * delta**2
-            body["v"][n] = (body["r"][n] - body["r"][n-1]) / (2 * delta)
-
-        n += 1
-        t += delta
-    
-    return bodies
+@jit(nopython=False)
+def verlet_backend(delta, limit, bodies, action=None, modifier=None, t=0.0, action_frequency=200):
+    return jit(verlet_nojit_backend, nopython=True)(delta, limit, bodies, action=action, modifier=modifier, t=t, action_frequency=action_frequency)
