2b4111a3b58182499a8b04612e80b1f3c70880ae
Jewels <jewels286@proton.me>
2025-01-11 15:39:11 -0500

    something?

--- axinite-1.12.1/axinite/tools/frontends/mpl.py
+++
@@ -1,5 +0,0 @@
-import axinite.tools as axtools
-import matplotlib.pyplot as ply
-
-def mpl_frontend(args: axtools.Body, mode: str):
-    if mode != "show": raise Exception("mpl_frontend is only supported in show mode")
